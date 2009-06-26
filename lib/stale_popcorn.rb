d7dfe51e2de413e3e47627e283e665d2116333d4
jsmestad <justin.smestad@gmail.com>
2009-06-26 15:18:08 -0600

    basic release

--- lib/stale_popcorn.rb
+++ lib/stale_popcorn.rb
@@ -3,6 +3,7 @@ require 'ftools'
 require 'yaml'
 require 'rubygems'
 require 'activesupport' # only used for time helpers
+require 'rio'
 
 module StalePopcorn
   # no one likes stale popcorn.
@@ -23,6 +24,9 @@ module StalePopcorn
   def self.update_stale(options={})
     # check each file for update
     load_yaml unless @yaml
+    stale = flag_stale
+    process(stale)
+    write_yaml
   end
 
   def self.load_yaml
@@ -39,10 +43,41 @@ protected
   def self.check_syntax
     raise YAML::Error, 'missing stale root element' unless @yaml['stale']
     @yaml['stale'].each do |key, value|
-      %w{ filepath frequency source updated }.each do |field|
-        raise YAML::Error, "missing #{field} node for #{key}" unless key[field]
+      %w{ filepath frequency source }.each do |field|
+        raise YAML::Error, "missing #{field} node for #{key}" unless @yaml['stale'][key][field]
       end
     end
   end
+
+  def self.flag_stale
+    stale = {}
+    scope = @yaml['stale']
+    scope.each do |key, value|
+      if scope[key]['updated'].blank?
+        stale.merge!({key => scope[key]})
+      else
+        last_modified = scope[key]['updated']
+        update_on = DateTime.now + eval(scope[key]['frequency'])
+        stale.merge!({key => scope[key]}) if last_modified > update_on
+      end
+    end
+    return stale
+  end
+
+  def self.process(fixtures)
+    p fixtures
+    fixtures.each do |key, value|
+      rio(fixtures[key]['source']) > rio(fixtures[key]['filepath'])
+      @yaml['stale'][key]['updated'] = DateTime.now
+    end
+    true
+  end
+
+  def self.write_yaml
+    File.open(@config_path, "w+") do |f|
+      f.write(@yaml.to_yaml)
+    end
+  end
+  
 
 end
--- spec/stale_popcorn_spec.rb
+++ spec/stale_popcorn_spec.rb
@@ -14,11 +14,31 @@ describe "StalePopcorn" do
       lambda { StalePopcorn.load_yaml }.should_not raise_error(Errno::ENOENT)
       lambda { StalePopcorn.load_yaml }.should raise_error(YAML::Error)
     end
+
+    it "should pass on valid yaml" do
+      StalePopcorn.config_path = File.dirname(__FILE__) + "/fixtures/stale_popcorn.yml"
+      lambda { StalePopcorn.load_yaml }.should_not raise_error(YAML::Error)
+    end
   end
 
   context "after loading the config" do
-    it "should update all stale fixtures"
-    it "should update only the passed fixtures, if stale"
-    it "should update all passed fixtures, when passed the :force flag"
+    before do
+      FileUtils.cp(File.dirname(__FILE__)+"/fixtures/stale_popcorn.yml", File.dirname(__FILE__)+"/fixtures/stale_popcorn.orig.yml")
+      StalePopcorn.config_path = File.dirname(__FILE__) + "/fixtures/stale_popcorn.yml"
+      StalePopcorn.valid_path?.should == true
+    end
+
+    it "should update all stale fixtures" do
+      StalePopcorn.update_stale.should == true
+    end
+
+    #it "should update only the passed fixtures, if stale"
+    #it "should update all passed fixtures, when passed the :force flag"
+    #it "should notify user, and rollback, if source is no longer valid"
+
+    after do
+      FileUtils.mv(File.dirname(__FILE__)+"/fixtures/stale_popcorn.yml", File.dirname(__FILE__)+"/../tmp/stale_popcorn.test.yml")
+      FileUtils.mv(File.dirname(__FILE__)+"/fixtures/stale_popcorn.orig.yml", File.dirname(__FILE__)+"/fixtures/stale_popcorn.yml")      
+    end
   end
 end
