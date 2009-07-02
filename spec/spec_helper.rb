680d911fc096655968c6a8c9ec6fcaba7c6a76c1
jsmestad <justin.smestad@gmail.com>
2009-07-01 20:18:33 -0600

    updated spec_helper with new app name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'stale_popcorn'
+require 'stale_fish'
 
 Spec::Runner.configure do |config|
   
