41dec171c080e2730928af4b2c8731ac3940f340
jsmestad <justin.smestad@gmail.com>
2009-10-02 16:17:59 -0600

    eval file_path

--- lib/stale_fish/fixture_definition.rb
+++ lib/stale_fish/fixture_definition.rb
@@ -11,7 +11,7 @@ module StaleFish
     def update!
       begin
         self.response = StaleFish.http.resource(source_url).get
-        File.open(file_path, 'w') { |file| file.write(response.body.to_s) }
+        File.open(eval(file_path), 'w') { |file| file.write(response.body.to_s) }
         self.last_updated_at = DateTime.now
       rescue Resourceful::UnsuccessfulHttpRequestError
         raise StaleFish::FixtureUpdateFailure, "#{key}'s source: #{value['source']} returned unsuccessfully."
